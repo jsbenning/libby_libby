e970f048c6b90aeb21bc05a996d5a27f8a1d6755
Joe <jsbenning4@gmail.com>
2016-10-23 23:35:25 -0400

    autogenerated controller helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'trades/show'
+
+  get 'messages/new'
+
+  get 'messages/show'
+
+  get 'books/new'
+
+  get 'books/show'
+
+  get 'books/index'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
