f3c72578112768bcf1196fb3d270462538b58b49
Joe <jsbenning4@gmail.com>
2016-10-23 23:51:37 -0400

    added relationships to book, genre, user models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :user, inverse_of: :books
+  validates_presence_of :user
   has_many :book_genres
   has_many :genres, through: :book_genres
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :books, inverse_of: :user
+  # has_many :bookgenres, through: :books (not sure about this)
+  accepts_nested_attributes_for :books
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
