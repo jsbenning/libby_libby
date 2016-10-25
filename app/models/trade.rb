b8f9f1f0600b21cbdb5447af6e5905c01c1ff07b
Joe <jsbenning4@gmail.com>
2016-10-25 11:49:50 -0400

    added helper methods to trade model

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -3,5 +3,25 @@ class Trade < ApplicationRecord
   belongs_to :requester, :class_name => 'User', :foreign_key => 'requester_id'
   has_one :initial_book, :class_name => 'Book', :foreign_key => 'initial_book_id'
   has_one :matched_book, :class_name => 'Book', :foreign_key => 'matched_book_id'
- 
+
+  def self.trades_requested(user)
+    coll = Array.new
+    Trade.each do |trade|
+      if trade.owner_id == user.id
+        coll << trade
+      end
+    end
+    coll
+  end
+
+  def self.trades_received(user)
+    coll = Array.new
+    Trade.each do |trade|
+      if trade.requester_id == user.id
+        coll << trade
+      end
+    end
+    coll
+  end
+
 end
