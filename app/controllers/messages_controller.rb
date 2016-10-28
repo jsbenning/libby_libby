9f9f929379ce9b39c20dc1f6ceb407806c6d17a7
Joe <jsbenning4@gmail.com>
2016-10-28 14:37:06 -0400

    added methods to controllers

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,11 @@
 class MessagesController < ApplicationController
+  def index
+  end
+  
   def new
+  end
+
+  def create
   end
 
   def show
--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,4 +1,54 @@
 class TradesController < ApplicationController
-  def show
+  def index
+    @trades_requested = Trade.trades_requested(current_user)
+    @trades_received = Trade.trades_received(current_user)
   end
+  
+  def new
+  end
+
+  def create
+    @trade = Trade.new(trade_params)
+  end
+
+  def edit
+    @trade = Trade.find(params[:id])
+  end
+
+  def update
+    @trade = Trade.find(params[:id])
+    if @trade.update(trade_params)
+      redirect_to "/trades"
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def trade_params
+    params.require(:trade).premit(:owner_id, :requester_id, :initial_book_id, :matched_book_id, :status)
+  end
+
 end
+
+
+    create_table :trades do |t|
+      t.integer :owner_id
+      t.integer :requester_id
+      t.integer :initial_book_id
+      t.integer :matched_book_id
+      t.string :status, default: "pending" #or 'complete'
+
+
+        def edit
+    @attraction = Attraction.find(params[:id])
+  end
+
+  def update
+    @attraction = Attraction.find(params[:id])
+    if @attraction.update_attributes(attraction_params)
+      redirect_to "/attractions/#{@attraction.id}"
+    else
+      render 'edit'
+    end
+  end
