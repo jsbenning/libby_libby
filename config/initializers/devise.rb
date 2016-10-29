0a92e371a279999e7bf0b4397ecb7003bf6d2f41
Joe <jsbenning4@gmail.com>
2016-10-29 15:25:36 -0400

    modified files for devise omniauth functionality

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -241,8 +241,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-
+  config.omniauth :facebook, '1169343939807509', '8a8751bd19c70d8ed98db0ac9eae41c7'
+  #callback_url: http://127.0.0.1
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,23 @@ Rails.application.routes.draw do
 
   root "home#index"
 
+
   resources :trades, only: [:index, :new, :create, :edit, :update]
 
   resources :messages, only: [:index, :new, :create]
 
-  devise_for :users, :path => 'accounts'
+  devise_for :users, :path => 'accounts', controllers: {
+    :omniauth_callbacks => 'users/omniauth_callbacks'
+  }
 
-  resources :users do
-    resources :books, only: [:index, :new, :create]
+  resources :users, only: [:show] do
+    resources :books, :except => [:index] do
+      get :index, :on => :collection, :action => 'index_users'
+    end
   end
-  resources :books, only: [:show, :edit, :update, :destroy]
+
+  resources :books, only: [:index_all], :except => [:index] do
+    get :index, :on => :collection, :action => 'index_all'
+  end
   
 end
