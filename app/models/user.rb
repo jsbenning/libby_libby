570f2da8ba43acd567a81be3abc9d065f70d95fe
Joe <jsbenning4@gmail.com>
2016-10-29 21:31:22 -0400

    protected user model method from divbyzero error

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,33 +2,37 @@ class User < ApplicationRecord
   has_many :books, inverse_of: :user
   # has_many :bookgenres, through: :books (not sure about this)
   accepts_nested_attributes_for :books
-  has_many :reviews
   
-  has_many :requests, :class_name => 'Trade', :foreign_key => 'requester_id'
-  has_many :trades, :class_name => 'Trade', :foreign_key => 'owner_id'
-
-  has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
-  has_many :received_messages, :class_name => 'Message', :foreign_key => 'recipient_id'
-
+  has_many :trades
+  has_many :messages
   
+  has_many :owners, :class_name => 'User', :through => :trades
+  has_many :requesters, :class_name => 'User', :through => :trades
+
+  has_many :senders, :class_name => 'User', :through => :messages
+  has_many :recipients, :class_name => 'User', :through => :messages
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
 
-  def user_rating
+  def present_user_rating
     x = Array.new
     self.reviews.each do |review|
       x << review.rating
     end
-    user_rating = x.inject(0){|sum, x| sum + x} / x.count
+    if x.count > 0
+      present_user_rating = x.inject(0){|sum, x| sum + x} / x.count
+    end
   end
 
   def self.from_omniauth(auth)
-  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-    user.email = auth.info.email
-    user.password = Devise.friendly_token[0,20]
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
   end
 
   # def self.new_with_session(params, session)
@@ -40,3 +44,9 @@ class User < ApplicationRecord
   # end
            
 end
+
+# has_many :requests, :class_name => 'Trade', :foreign_key => 'requester_id'
+# has_many :trades, :class_name => 'Trade', :foreign_key => 'owner_id'
+
+# has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
+# has_many :received_messages, :class_name => 'Message', :foreign_key => 'recipient_id'
