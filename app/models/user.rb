7deac9b928bdb7ab277d9c84c4ecfa1b1128c9de
Joe <jsbenning4@gmail.com>
2016-11-01 23:14:07 -0400

    cleaned up user model, removed comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :books, inverse_of: :user
-  # has_many :bookgenres, through: :books (not sure about this)
   accepts_nested_attributes_for :books
 
   has_many :trades
@@ -8,9 +7,6 @@ class User < ApplicationRecord
   has_many :owners, :class_name => 'User', :through => :trades
   has_many :requesters, :class_name => 'User', :through => :trades
 
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
@@ -28,19 +24,5 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0,20]
     end
   end
-
-  # def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-  #       user.email = data["email"] if user.email.blank?
-  #     end
-  #   end
-  # end
            
 end
-
-# has_many :requests, :class_name => 'Trade', :foreign_key => 'requester_id'
-# has_many :trades, :class_name => 'Trade', :foreign_key => 'owner_id'
-
-# has_many :sent_messages, :class_name => 'Message', :foreign_key => 'sender_id'
-# has_many :received_messages, :class_name => 'Message', :foreign_key => 'recipient_id'
