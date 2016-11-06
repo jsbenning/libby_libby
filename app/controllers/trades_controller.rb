1c5fb54fa444bf2b8965abb3b991d1c095397d05
Joe <jsbenning4@gmail.com>
2016-11-05 22:07:43 -0400

    added styles

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,7 +2,6 @@ class TradesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    binding.pry
     @user = current_user
     @user_needs_response_trades = Trade.user_needs_response(@user)
     @user_must_complete_trades = Trade.user_must_complete(@user)
@@ -10,22 +9,13 @@ class TradesController < ApplicationController
     @completed_by_other_trades = Trade.completed_by_other(@user)
   end
   
-  # def new
-  #   @trade =
-  # end
-
-  # def show
-  #   redirect_to '/trades/index'
-  # end
-
   def create
-    binding.pry
     @trade = Trade.new(trade_params)
     if @trade.save
         initial_book = Book.find(@trade.initial_book_id)
         initial_book.status = "traded"
         initial_book.save
-        flash[:notice] = "You've initiated a new trade. Please wait for a response soon."
+        flash[:notice] = "You've just initiated a new trade! Please wait for a response soon."
       redirect_to action: "index"
     else
       flash[:notice] = 'There was a problem creating a trade!'
@@ -33,9 +23,6 @@ class TradesController < ApplicationController
     end
   end
 
-  # def edit
-  #   @trade = Trade.find(params[:id])
-  # end
 
   def update
     @trade = Trade.find(params[:id])
@@ -55,13 +42,15 @@ class TradesController < ApplicationController
     @trade = Trade.find(params[:id])
     initial_book = Book.find(@trade.initial_book_id)
     initial_book.status = "at_home"
-    matched_book = Book.find(@trade.matched_book_id)
-    matched_book.status = "at_home"
+    if @trade.matched_book_id
+      matched_book = Book.find(@trade.matched_book_id)
+      matched_book.status = "at_home"
+      matched_book.save
+    end
     initial_book.save
-    matched_book.save
     @trade = nil
     flash[:notice] = 'Trade deleted!'
-    redirect_to '/home/index'
+    redirect_to root_path
   end
 
   private
