a48f33069408cfa35290c57d9074a07a8011341b
Joe <jsbenning4@gmail.com>
2016-11-12 13:32:38 -0500

    adjusted reviews migration

--- db/migrate/20161029021158_create_reviews_users.rb
+++ db/migrate/20161029021158_create_reviews_users.rb
@@ -1,8 +1,8 @@
 class CreateReviewsUsers < ActiveRecord::Migration[5.0]
   def change
     create_join_table :reviews, :users do |t|
-      t.index :reviewer_id
-      t.index :reviewee_id
+      t.index :review_id
+      t.index :user_id
     end
   end
 end
