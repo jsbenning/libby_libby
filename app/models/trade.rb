b352b77dabf85168f0356e514d5a02a0225fce4a
Joe <jsbenning4@gmail.com>
2016-12-15 20:31:50 -0500

    revised join table associations

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -1,7 +1,7 @@
 class Trade < ApplicationRecord
 
-  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
   belongs_to :requester, :class_name => 'User', :foreign_key => 'requester_id'
+  belongs_to :owner, :class_name => 'User', :foreign_key => 'owner_id'
   has_one :initial_book, :class_name => 'Book', :foreign_key => 'initial_book_id'
   has_one :matched_book, :class_name => 'Book', :foreign_key => 'matched_book_id'
 
