3a78258a197f2cab13e5f3b27d5d68030e6f18b7
Joe <jsbenning4@gmail.com>
2017-07-22 14:29:24 -0400

    updated routing and controllers to add devise sessions to users directory

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,13 @@ Rails.application.routes.draw do
 
   resources :trades
 
-  devise_for :users, :path => 'accounts', controllers: {
-    :omniauth_callbacks => 'users/omniauth_callbacks'
-  }
+  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => 'users/omniauth_callbacks' }
+
+
+
+  # devise_for :users, :path => 'accounts', controllers: {
+  #   :omniauth_callbacks => 'users/omniauth_callbacks'
+  # }
 
   resources :users, only: [:show, :index, :edit, :update, :destroy] do
     resources :books, :except => [:index] do
