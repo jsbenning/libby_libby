cdb3886cfdc03f8da29638cc9076341996971d95
Joe <jsbenning4@gmail.com>
2017-07-22 16:40:19 -0400

    updated registration and routes to redirect to user show page

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,13 +1,9 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-#before_action :configure_sign_up_params, only: [:create]
-#before_action :configure_account_update_params, only: [:update]
+before_action :configure_sign_up_params, only: [:create]
+before_action :configure_account_update_params, only: [:update]
+
 
   #GET /resource/sign_up
-
-  def build_resource(params)
-    super(resource_params)
-  end
-
   def new
     super
   end
@@ -41,23 +37,18 @@ class Users::RegistrationsController < Devise::RegistrationsController
     super
   end
 
-  private
-
-  def resource_params
-    params.require(:user).permit(:real_name, :street, :city, :state, :zipcode, :role)
-  end
 
   protected
 
   #If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:real_name, :street, :city, :state, :zipcode]))
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:real_name, :street, :city, :state, :zipcode])
+  end
 
   #If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:real_name, :street, :city, :state, :zipcode]))
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:real_name, :street, :city, :state, :zipcode])
+  end
 
   #The path used after sign up.
   def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :trades
 
-  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations', :omniauth_callbacks => 'users/omniauth_callbacks' }
 
 
 
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   #   :omniauth_callbacks => 'users/omniauth_callbacks'
   # }
 
-  resources :users, only: [:show, :index, :edit, :update, :destroy] do
+  resources :users, only: [:show, :index, :edit, :update, :destroy] do  
     resources :books, :except => [:index] do
       get :index, :on => :collection, :action => 'index_users'
     end
