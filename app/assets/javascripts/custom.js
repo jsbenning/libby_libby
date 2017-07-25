034d7a7787212931ec5f537445e78ab5d372147e
Joe <jsbenning4@gmail.com>
2017-07-24 23:17:21 -0400

    added spec-js.md file

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -10,15 +10,18 @@ $(document).ready(function(){
   });
 
 
+// #Profile Functions
 
-
-  $("#profile").on('click', function() {  
+  $("#profile").on('click', function(e) { 
+    $("#profile").attr('disabled', 'disabled'); 
     var personId = this.getAttribute('data-id');
     var url = "http://localhost:3000/users/" + personId + ".json";
+    var html = "<div class='boxframe'><h2>Your Profile: </h2>"
     $.ajax({
       dataType: "json",
       url: url,
       success: function(data) {
+        $("#profile").removeAttr('disabled');
         html += '<p><strong>Your Real Name: ' + data['real_name'] + '</strong></p>'
         html += '<p><strong>Your Street Address: ' + data['street'] + '</strong></p>'
         html += '<p><strong>Your City: ' + data['city'] + '</strong></p>'
@@ -27,8 +30,13 @@ $(document).ready(function(){
         html += '<p><strong>Your Role: ' + data['role'] + '</strong></p>'
         html += '</div>';
         $("#myProfile").append(html);
+      },
+      error: function() {
+        console.log("sumpin broke");
       }
     });
+    e.stopImmediatePropagation();
+    return false;
   });
 
 
