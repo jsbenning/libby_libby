5f85f4b1d0af01fe5637dae4745225f16a1e8463
Joe <jsbenning4@gmail.com>
2017-07-24 22:45:49 -0400

    fleshed out serializers

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,7 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :user_id, :title, :author_last_name, :author_first_name, :isbn, :condition, :description, :status
+  has_and_belongs_to_many :genres
+  belongs_to :user
+
 end
+
--- app/serializers/genre_serializer.rb
+++ app/serializers/genre_serializer.rb
@@ -1,3 +1,4 @@
 class GenreSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name
+  has_and_belongs_to_many :books
 end
