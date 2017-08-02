0ceecede72f7f2243033ae027921ba2e5ff76cbe
Joe <jsbenning4@gmail.com>
2017-08-02 16:22:55 -0400

    updated showbook js and template

--- app/assets/javascripts/showBook.js
+++ app/assets/javascripts/showBook.js
@@ -5,13 +5,25 @@ $(document).ready(function(){
     var userId = $(this).attr('data-value1');
     var bookId = $(this).attr('data-value2');
     var url = "http://localhost:3000/users/" + userId + "/books/" + bookId + ".json";
+    // function Book(data) {
+
+    // }
 
     $.ajax({
       dataType: "json",
       url: url,
       success: function(data) {
+
+        function extend(obj) {
+          obj.authorFullName = function() {
+            return this.author_first_name + " " + this.author_last_name
+          }
+        }
+        extend(data);
+        alert("hoo");
+        data.authorFullName = data.authorFullName();
         bookHtml = HandlebarsTemplates['showBookTemplate'] ({
-          book: data.book
+          book: data
         });
       $('#display-area').html(bookHtml);
       },
@@ -23,3 +35,6 @@ $(document).ready(function(){
     return false;
   });
 });
+
+
+
