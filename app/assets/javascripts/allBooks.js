34514ad66f19880fe7e4532fa6b316b6f6faf675
Joe <jsbenning4@gmail.com>
2017-08-02 16:20:32 -0400

    updated allbooks js and template

--- app/assets/javascripts/allBooks.js
+++ app/assets/javascripts/allBooks.js
@@ -6,16 +6,16 @@ $(document).ready(function(){
     var searchButton = "<form accept-charset='UTF-8' action='/books' method='get'><input name='search' type='text' id='search' size='50' placeholder='Enter keyword(title, author, ISBN) here to search'/><br><br><input type='submit' class='btn btn-primary' value='Search' /></form>"
 
     var url = "http://localhost:3000/books.json";
-    
+
     $.ajax({
       dataType: "json",
       url: url,
       success: function(data) {
         $("#all-books-btn").removeAttr('disabled');
-        booksListHtml = HandlebarsTemplates['allBooksTemplate'] ({
+        allBooksHtml = HandlebarsTemplates['allBooksTemplate'] ({
           books: data
         });
-        $('#display-area').html(booksListHtml);
+        $('#display-area').html(allBooksHtml);
       },
       error: function() {
         console.log("Sumpin broke");
