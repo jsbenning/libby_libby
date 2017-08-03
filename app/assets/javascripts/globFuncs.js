0226c89d9328de99e2c7bc79c02325462851e022
Joe <jsbenning4@gmail.com>
2017-08-03 16:12:37 -0400

    renamed js file holding global functions

---
+++ app/assets/javascripts/globFuncs.js
@@ -0,0 +1,10 @@
+var clearDivs = function() {
+  $("#display-area").html('');
+  $("#msg-area").html('');
+}
+
+function extend(obj) {
+  obj.authorFullName = function() {
+    return this.author_first_name + " " + this.author_last_name
+  }
+}
