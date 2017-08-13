f4b2d126160739affc9a110ef6f57fd0ee434dde
Joe <jsbenning4@gmail.com>
2017-08-12 22:24:18 -0400

    added ar association with foreign key to books

--- app/assets/javascripts/trades.js
+++ app/assets/javascripts/trades.js
@@ -5,10 +5,7 @@ $(document).ready(function(){
   $(document.body).on('click', '#my-trades-btn', function(e){
     $("#my-trades-btn").attr('disabled', 'disabled');
     clearDivs();
-
-    //var personId = this.getAttribute('data-id');
-    var url = "http://localhost:3000/trades.json";
-    
+    var url = "http://localhost:3000/trades.json"; 
     $.ajax({
       dataType: "json",
       url: url,
@@ -31,10 +28,10 @@ $(document).ready(function(){
   });
 
 
-  // View other trader's books to complete a trade
+  // View other trader's books to possibly complete a trade
 
-  $(document.body).on('click', '#see-books-btn', function(e){
-    $("#see-books-btn").attr('disabled', 'disabled');
+  $(document.body).on('click', '.see-books-btn', function(e){
+    $(".see-books-btn").attr('disabled', 'disabled');
     clearDivs();
     var userId = $(this).data('user');
     var url = "http://localhost:3000/users/" + userId + "/books.json";
@@ -42,7 +39,7 @@ $(document).ready(function(){
       dataType: "json",
       url: url,
       success: function(data) {
-        $("#my-books-btn").removeAttr('disabled');
+        $(".see-books-btn").removeAttr('disabled');
         // the following conditional determines whether current_user has any books
         if (data.books[0].id) {
           var usersBooksHtml = HandlebarsTemplates['allBooksTemplate'] ({
@@ -63,8 +60,6 @@ $(document).ready(function(){
   });
 
 
-
-
 // var x = $(document.querySelector('input[name=user_review]:checked')).val();
 // var rating = parseInt(x);
 // <button type="button" class="btn btn-primary btn-xs" {data-trader1={{trade.first_trader_id}} data-trader2={{book.user_id}} 
@@ -75,11 +70,12 @@ $(document).ready(function(){
   $(document.body).on('click', '#request-trade-btn', function(e){
     $('#request-trade-btn').attr('disabled', 'disabled');
     clearDivs();
+    var title =$(this).data('title');
     var bookFirstTraderWantsId = $(this).data('book');
     var firstTraderId = $(this).data('trader1'); 
     var secondTraderId = $(this).data('trader2');
     var url = "http://localhost:3000/trades";
-    //console.log(bookId);
+    console.log(firstTraderId);
     var myData = { trade: { book_first_trader_wants_id: bookFirstTraderWantsId, first_trader_id: firstTraderId, second_trader_id: secondTraderId }};
       $.ajax({
         dataType: "json",
@@ -98,6 +94,115 @@ $(document).ready(function(){
       e.stopImmediatePropagation();
         return false;
     });
+
+
+  // Complete a trade, current_user chooses second book as second trader
+
+  $(document.body).on('click', '.complete-trade-btn', function(e){
+    $('.complete-trade-btn').attr('disabled', 'disabled');
+    clearDivs();
+    var bookSecondTraderWantsId = $(this).data('book');
+    var tradeId = $(this).data('trade');
+    var url = "http://localhost:3000/trades/" + tradeId + ".json";
+    var myData = { trade: { book_second_trader_wants_id: bookSecondTraderWantsId }};
+      $.ajax({
+        dataType: "json",
+        type: "PATCH",
+        url: url,
+        data:  myData,
+        success: function(data) {
+          clearDivs();
+          $(".complete-trade-btn").removeAttr('disabled');
+          $('.notice').html(data.msg);
+        },
+        error : function() {
+          console.log("Sumpin broke!");
+        }
+      });
+      e.stopImmediatePropagation();
+        return false;
+    });
+
+  // Rate trader 1, if current_user is trader 2
+
+  $(document.body).on('click', '.rate-trader1-btn', function(e){
+    $('.rate-trader1-btn').attr('disabled', 'disabled');
+    clearDivs();
+    var tradeId = $(this).data('trade');
+    var x = $(document.querySelector('input[name=user_review]:checked')).val();
+    var rating = parseInt(x);
+    var url = "http://localhost:3000/trades/" + tradeId + ".json";
+    var myData = { trade: { first_trader_rating: rating }};
+      $.ajax({
+        dataType: "json",
+        type: "PATCH",
+        url: url,
+        data:  myData,
+        success: function(data) {
+          clearDivs();
+          $('.rate-trader1-btn').removeAttr('disabled');
+          $('.notice').html(data.msg);
+        },
+        error : function() {
+          console.log("Sumpin broke!");
+        }
+      });
+      e.stopImmediatePropagation();
+        return false;
+    });
+
+  // Rate trader 2, if current_trader is trader 1
+
+  $(document.body).on('click', '.rate-trader2-btn', function(e){
+    $('.rate-trader2-btn').attr('disabled', 'disabled');
+    clearDivs();
+    var tradeId = $(this).data('trade');
+    var x = $(document.querySelector('input[name=user_review]:checked')).val();
+    var rating = parseInt(x);
+    var url = "http://localhost:3000/trades/" + tradeId + ".json";
+    var myData = { trade: { second_trader_rating: rating }};
+      $.ajax({
+        dataType: "json",
+        type: "PATCH",
+        url: url,
+        data:  myData,
+        success: function(data) {
+          clearDivs();
+          $('.rate-trader2-btn').removeAttr('disabled');
+          $('.notice').html(data.msg);
+        },
+        error : function() {
+          console.log("Sumpin broke!");
+        }
+      });
+      e.stopImmediatePropagation();
+        return false;
+    });
+
+    $(document.body).on('click', '.cancel-trade-btn', function(e){
+    $('.cancel-trade-btn').attr('disabled', 'disabled');
+    clearDivs();
+    var tradeId = $(this).data('trade');
+    var url = "http://localhost:3000/trades/" + tradeId + ".json";
+    var myData = { trade: { id: tradeId }};
+      $.ajax({
+        dataType: "json",
+        type: "DELETE",
+        url: url,
+        data:  myData,
+        success: function(data) {
+          clearDivs();
+          $('.cancel-trade-btn').removeAttr('disabled');
+          $('.notice').html(data.msg);
+        },
+        error : function() {
+          console.log("Sumpin broke!");
+        }
+      });
+      e.stopImmediatePropagation();
+        return false;
+    });
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804011731) do
+ActiveRecord::Schema.define(version: 20170813022219) do
 
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170804011731) do
     t.string   "status",            default: "at_home"
     t.datetime "created_at",                            null: false
     t.datetime "updated_at",                            null: false
+    t.integer  "trade_id"
   end
 
   create_table "books_genres", id: false, force: :cascade do |t|
