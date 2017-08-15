eb423fd1e5a601c851d5fc08923c198634292f21
Joe <jsbenning4@gmail.com>
2017-08-15 13:48:52 -0400

    commented ot fields from bookk serializer for testing

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,6 +2,7 @@ class BookSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :title, :author_last_name, :author_first_name, :isbn, :condition, :description, :status, :genres
   #has_many :genres
   belongs_to :user
+  #belongs_to :trade
 
   def genres
     object.genres.map do |genre|
